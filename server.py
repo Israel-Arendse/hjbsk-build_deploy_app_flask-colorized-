be58d79ca4b1c17cd2f96bad5b2e542a23512f59
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-09-16 06:41:00 -0400

    Update ValueError message

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
-# Import the Maths package here
-from Maths.mathematics import summation, subtraction, multiplication
+from flask import Flask, render_template, request # Import Flask, render_template, and request
+from Maths.mathematics import summation, subtraction, multiplication # Import summation, subtraction, and multiplcation
 
 app = Flask("Mathematics Problem Solver")
 
@@ -10,11 +9,10 @@ def sum_route():
     try:
         num1 = float(request.args.get('num1'))
         num2 = float(request.args.get('num2'))
-         # Write your code here
         result = summation(num1, num2)
         return str(result)
     except ValueError:
-        return "Plase add an number"
+        return "Please enter valid numeric values for both 'Number 1' and 'Number 2'."
 
 
 # Define app route for the subtraction function
@@ -23,27 +21,24 @@ def sub_route():
     try:
         num1 = float(request.args.get('num1'))
         num2 = float(request.args.get('num2'))
-        # Write your code here
         result = subtraction(num1, num2)
         return str(result)
     except ValueError:
-        return "Please add an number"
+        return "Please enter valid numeric values for both 'Number 1' and 'Number 2'."
 
 
 # Define app route for the multiplication function
 @app.route("/mul")
 def mul_route():
         num1 = float(request.args.get('num1'))
-        num2 = float(request.args.get('num2'))
-        # Write your code here  
+        num2 = float(request.args.get('num2'))  
         result = multiplication(num1, num2)
         return str(result)
     except ValueError:
-        return "Please add an number"
+        return "Please enter valid numeric values for both 'Number 1' and 'Number 2'."
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
     return render_template('index.html')
 
     
