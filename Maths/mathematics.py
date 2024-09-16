7c0a4332d343ae938febadf335d84d14ce847564
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-09-16 02:30:38 -0400

    Created 'mathematics.py' file

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+def summation(a, b):
+    resutl = a + b
+    return result
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+def multiplication(a, b):
+    result = a * b
+    return result
